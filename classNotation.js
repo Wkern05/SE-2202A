bede68d15f78a3f236e900709fe5f56f500cf620
Wkern05 <wolfkern05@gmail.com>
2025-11-17 00:00:17 -0500

    Add files via upload

---
+++ classNotation.js
@@ -0,0 +1,60 @@
+/**
+ * Define the Course and Assignment concepts using the class notations
+ */
+
+/*
+ * assignment class
+ * creates an assignment with a title and a due date
+ */
+class Assignment{
+    //constructor for Assignment
+    constructor(title, dueDate){
+        //initialize object properties
+        this.title = title
+        this.dueDate = dueDate
+    }
+
+    //method automatically added to Assignment.prototype
+    printAssignment(){
+        console.log('   Title: ' + this.title + ' | Due Date: ' + this.dueDate);
+    }
+}
+
+/*
+ * course class
+ * creates a course with a name, instructor, creditHours, and list of assignments
+ */
+class Course{
+    //constructor for Course
+    constructor(courseName, instructor, creditHours, assignments){
+        this.courseName = courseName
+        this.instructor = instructor
+        this.creditHours = creditHours
+        this.assignments = assignments
+    }
+
+    //method automatically added to Course.prototype
+    courseInfo() {
+    console.log('Course: ' + this.courseName + 
+                ' | Instructor: ' + this.instructor + 
+                ' | Credit Hours: ' + this.creditHours);
+
+    console.log('Assignments >>>');
+
+    //loop through all assignments and print them
+    for (let a of this.assignments)
+        a.printAssignment();
+    }
+}
+// create the objects using the classes
+let a1 = new Assignment('Project Proposal','Jan 15');
+let a2 = new Assignment('Midterm Report','Feb 20');
+let a3 = new Assignment('Final Report','Mar 30');
+let a4 = new Assignment('Presentation','Apr 10');
+
+let c1 = new Course('Software Engineering', 'Dr. Pepper', 3, [a1, a2]);
+
+let c2 = new Course('Data Science', 'Dr. Evil', 6,[a3, a4]);
+
+c1.courseInfo();
+c2.courseInfo();
